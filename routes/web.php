25557536cbf76dd8b0fde180ecd3e043e8a2ef94
kuratea52 <yasaimogu@gmail.com>
2024-01-18 13:39:49 +0900

    Create Blog Post Form

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,11 @@ Route::get('/posts', [
     'index'
 ]);
 
+Route::get('/posts/create', [
+    PostController::class,
+    'create'
+]);
+
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostController.phpのshowメソッドを実行する。
 Route::get('/posts/{post}', [
     PostController::class,
