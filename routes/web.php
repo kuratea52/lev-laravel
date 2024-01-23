8092df70c34dc6e57c49f788cfbd02d1bcff382f
kuratea52 <yasaimogu@gmail.com>
2024-01-23 11:59:40 +0900

    Implementing Authentication Functionality

--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
-use App\Http\Controllers\CategoryController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [
-    PostController::class,
-    'index'
-]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/posts/create', [
-    PostController::class,
-    'create'
-]);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// '/posts'にPostリクエストが来たら、PostController.phpのstoreメソッドを実行する。
-Route::post('/posts', [
-    PostController::class,
-    'store'
-]);
-
-// '/posts/{対象データのID}'にGetリクエストが来たら、PostController.phpのshowメソッドを実行する。
-Route::get('/posts/{post}', [
-    PostController::class,
-    'show'
-]);
-
-Route::put('/posts/{post}', [
-    PostController::class,
-    'update'
-]);
-
-// '/posts/{対象データのID}'にDeleteリクエストが来たら、PostController.phpのdeleteメソッドを実行する。
-Route::delete('/posts/{post}', [
-    PostController::class,
-    'delete'
-]);
-
-Route::get('/posts/{post}/edit', [
-    PostController::class,
-    'edit'
-]);
-
-Route::get('/categories/{category}', [
-    CategoryController::class,
-    'index'
-]);
+require __DIR__.'/auth.php';
