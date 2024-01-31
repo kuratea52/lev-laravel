46831386830feb228ddba35b668d94bcfb5be937
kuratea52 <yasaimogu@gmail.com>
2024-01-31 21:30:47 +0900

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,8 +9,8 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(PostSeeder::class);
-        // \App\Models\User::factory(10)->create();
+        // $this->call(PostSeeder::class);
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
