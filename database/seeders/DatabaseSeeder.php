ebe9834862ed8804757fc84e818f7b90a8b66d9f
kuratea52 <yasaimogu@gmail.com>
2024-02-03 12:14:01 +0900

    Creating DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(PostSeeder::class);
         \App\Models\User::factory(10)->create();
+        \App\Models\Post::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
