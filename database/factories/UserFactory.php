0f7a86c75569eca4d0d036286cf2ede0d5a7a347
kuratea52 <yasaimogu@gmail.com>
2024-02-03 12:03:00 +0900

    Creating UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,8 +21,15 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'password' => bcrypt('password'), // 仮のパスワードをハッシュ化して使用する例
+            'bio' => $this->faker->text(200),
+            'post_count' => $this->faker->numberBetween(0, 50),
+            'following_count' => $this->faker->numberBetween(0, 100),
+            'followers_count' => $this->faker->numberBetween(0, 100),
             'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 
