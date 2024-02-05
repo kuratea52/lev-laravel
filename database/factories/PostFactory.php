67b272049142bbe69bbdb7d5b8b03d17cae2bc6b
kuratea52 <yasaimogu@gmail.com>
2024-02-05 12:15:08 +0900

    Modify the factory file for the Posts table

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -13,16 +13,13 @@ class PostFactory extends Factory
 
     public function definition()
     {
-        $regions = ['tokyo', 'kyoto'];
-        
         return [
             'user_id' => $this->faker->numberBetween(1, 10),
-            'region' => $this->faker->randomElement($regions),
-            'season' => $this->faker->word,
-            'participants' => $this->faker->numberBetween(1, 10),
+            'region' => $this->faker->randomElement(['北海道', '東北', '関東', '中部', '近畿', '中国', '四国', '九州', '沖縄']),
+            'season' => $this->faker->randomElement(['春', '夏', '秋', '冬']),
+            'participants' => $this->faker->randomElement(['１人', '２人', '３～５人', '６人～']),
             'budget' => $this->faker->randomElement(['～１万円', '～３万円', '～５万円', '～１０万円', '１０万円～']),
-            'stay_duration' => $this->faker->numberBetween(1, 14),
-            'transportation' => $this->faker->word,
+            'stay_duration' => $this->faker->randomElement(['１泊', '２泊', '３泊', '４泊～']),
             'title' => $this->faker->sentence(5),
             'content' => $this->faker->text(100),
             'is_public' => $this->faker->boolean(true),
