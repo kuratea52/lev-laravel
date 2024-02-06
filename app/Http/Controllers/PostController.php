9a631ce985cb5b7e0959356ded7c1b9e8bb31731
kuratea52 <yasaimogu@gmail.com>
2024-02-06 11:57:52 +0900

    Modified to receive each data in order of number of likes.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,16 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PostRequest;
 use App\Models\Post;
-use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
+        $totalLikesRanking = Post::getTotalLikesRanking();
         $kantoRanking = Post::getRankingByRegion('関東');
         $springRanking = Post::getRankingBySeason('春');
+        $oneParticipantLikesRanking = Post::getLikesRankingByParticipants('１人');
+        $budgetLikesRanking = Post::getLikesRankingByBudget('～３万円');
+        $dayTripLikesRanking = Post::getLikesRankingByStayDuration('日帰り');
         $posts = $post->getByLimit();
-        return view('index', compact('kantoRanking', 'springRanking', 'posts'));
+        
+        return view('index', compact(
+            'totalLikesRanking',
+            'kantoRanking',
+            'springRanking',
+            'oneParticipantLikesRanking',
+            'budgetLikesRanking',
+            'dayTripLikesRanking',
+            'posts'));
     }
     
     public function show(Post $post)
@@ -23,7 +35,8 @@ class PostController extends Controller
     
     public function store(Post $post, PostRequest $request)
     {
-        $input = $request['post'];   // PostRequestクラスの処理をした$requestからキーがpostのものを$inputに代入
+        // $input = $request['post'];   // PostRequestクラスの処理をした$requestからキーがpostのものを$inputに代入
+        $validatedData = $request->validated();
         $post->fill($input)->save();   // Postクラスのfillメソッドとsaveメソッドを呼び出している
         return redirect('/posts/' . $post->id);   // このURLはつまりshow.blade.phpと同じページを表示している
     }
@@ -35,7 +48,8 @@ class PostController extends Controller
     
     public function update(PostRequest $request, Post $post)
     {
-        $input_post = $request['post'];
+        // $input_post = $request['post'];
+        $validatedData = $request->validated();
         $post->fill($input_post)->save();
         return redirect('/posts/' . $post->id);
     }
