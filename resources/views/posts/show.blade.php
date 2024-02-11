5efce1bcf2d7cbc747601373d57fb014b8f70787
kuratea52 <yasaimogu@gmail.com>
2024-02-11 11:53:12 +0900

    fix/Modified to properly store is_public

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,7 +15,7 @@
                 <p><strong>予算:</strong> {{ $post->budget }}</p>
                 <p><strong>滞在期間:</strong> {{ $post->stay_duration }}</p>
                 @if(auth()->id() === $post->user_id)
-                    <p><strong>公開:</strong> {{ $post->is_public ? 'はい' : 'いいえ' }}</p>
+                    <p><strong>公開:</strong> {{ $post->is_public == 1 ? 'はい' : 'いいえ' }}</p>
                 @endif
                 <p><strong>いいね数:</strong> {{ $post->likes }}</p>
                 <p><strong>作成日時:</strong> {{ $post->created_at }}</p>
