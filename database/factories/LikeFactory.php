52a49dae1636f7ae789a41c687b0efe8c06b0b0b
kuratea52 <yasaimogu@gmail.com>
2024-02-13 11:36:49 +0900

    add/Add a factory file for the likes table

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Like;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LikeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Like::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => function () {
+                return \App\Models\User::factory()->create()->id;
+            },
+            'post_id' => function () {
+                return \App\Models\Post::factory()->create()->id;
+            },
+            // Add any additional fields as needed
+        ];
+    }
+}
