c673e92a148d7d1c516279086ef5d3dbbb812f8f
kuratea52 <yasaimogu@gmail.com>
2024-02-18 13:10:35 +0900

    add/Added process to display additional input fields after clicking "Additional Input".

--- resources/views/newpost.blade.php
+++ resources/views/newpost.blade.php
@@ -110,4 +110,21 @@
             </div>
         </form>
     </div>
+    
+    <script>
+        document.getElementById('add-more-fields').addEventListener('click', function() {
+            // フォームを追加
+            var additionalFields = document.getElementById('additional-fields');
+            var newFields = document.createElement('div');
+            var fieldIndex = document.querySelectorAll('[id^="place_visited_"]').length + 1;
+
+            newFields.innerHTML = `
+                <div class="mt-2 flex items-center">
+                    <input id="place_visited_${fieldIndex}" name="place_visited_${fieldIndex}" type="text" class="block w-1/2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="行った場所${fieldIndex}" />
+                    <textarea id="impressions_${fieldIndex}" name="impressions_${fieldIndex}" rows="2" class="block ml-2 w-1/2 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="感想${fieldIndex}"></textarea>
+                </div>
+            `;
+            additionalFields.appendChild(newFields);
+        });
+    </script>
 </x-app-layout>
