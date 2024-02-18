c91cd85228d375f7dde9caf82328a85394e1f23b
kuratea52 <yasaimogu@gmail.com>
2024-02-18 12:51:42 +0900

    fix/Fixed dummy data inserted in "place_visited_$i" and "impressions_$i

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -13,7 +13,7 @@ class PostFactory extends Factory
 
     public function definition()
     {
-        return [
+        $data = [
             'user_id' => $this->faker->numberBetween(1, 10),
             'region' => $this->faker->randomElement(['北海道', '東北', '関東', '中部', '近畿', '中国', '四国', '九州', '沖縄']),
             'season' => $this->faker->randomElement(['春', '夏', '秋', '冬']),
