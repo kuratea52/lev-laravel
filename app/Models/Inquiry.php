9a123008e18479b268ea604da4559486a6f9d777
kuratea52 <yasaimogu@gmail.com>
2024-02-23 11:46:10 +0900

    fix/Fix variable to be stored in $fillable

--- app/Models/Inquiry.php
+++ app/Models/Inquiry.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Inquiry extends Model
 {
     // use HasFactory;
-    protected $fillable = ['name', 'email', 'message'];
+    protected $fillable = ['category', 'email', 'body'];
 }
