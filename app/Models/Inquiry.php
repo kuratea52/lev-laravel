0dd6a5518702b3ba2b1be160e2adac725a5a5d02
kuratea52 <yasaimogu@gmail.com>
2024-02-23 13:02:31 +0900

    fix/Fixed user_id to store value when logging in

--- app/Models/Inquiry.php
+++ app/Models/Inquiry.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Inquiry extends Model
 {
     // use HasFactory;
-    protected $fillable = ['category', 'email', 'body'];
+    protected $fillable = ['user_id', 'category', 'email', 'body'];
 }
