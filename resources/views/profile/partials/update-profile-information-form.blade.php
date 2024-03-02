1c710a16030d4a0d0c28ab57f6a5bf99bb1e56fa
kuratea52 <yasaimogu@gmail.com>
2024-03-02 15:09:51 +0900

    fix/Changed messages in if statements to Japanese

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -27,16 +27,16 @@
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
+                        {{ __('入力されたメールアドレスは認証されていません。') }}
 
                         <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
+                            {{ __('こちらをクリックして認証メールを再送信してください。') }}
                         </button>
                     </p>
 
                     @if (session('status') === 'verification-link-sent')
                         <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
+                            {{ __('新しい認証リンクがメールアドレスに送信されました。') }}
                         </p>
                     @endif
                 </div>
