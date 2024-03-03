fc2360217c0df720c0f964682ceaa92564178d3e
kuratea52 <yasaimogu@gmail.com>
2024-03-03 17:54:16 +0900

    fix/Changed to Japanese

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,8 @@
+@section('title', 'パスワードの再設定')
+
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('ご登録頂いたメールアドレスを入力してください。パスワードリセットリンクをお送りいたします。') }}
     </div>
 
     <!-- Session Status -->
@@ -11,14 +13,14 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('メールアドレス')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('パスワードリセットリンクを送信') }}
             </x-primary-button>
         </div>
     </form>
