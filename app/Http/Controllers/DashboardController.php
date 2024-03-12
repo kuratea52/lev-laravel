f950abaec86c5a75cb61f9ad5c73cda840dae928
kuratea52 <yasaimogu@gmail.com>
2024-03-12 12:17:09 +0900

    add/Add details

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,14 +10,14 @@ class DashboardController extends Controller
     public function myposts()
     {
         $user = auth()->user();
-        $posts = Post::where('user_id', $user->id)->get();
+        $posts = Post::where('user_id', $user->id)->paginate(9);
         return view('dashboard.myposts', compact('posts'));
     }
     
     public function likeposts()
     {
         $user = auth()->user();
-        $posts = Post::where('user_id', $user->id)->get();
+        $posts = Post::where('user_id', $user->id)->paginate(9);
         return view('dashboard.likeposts', compact('posts'));
     }
 }
