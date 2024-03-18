5c78ee1b69fa95b574cb99f67d6f5411f69ff5b0
kuratea52 <yasaimogu@gmail.com>
2024-03-18 16:12:01 +0900

    add

---
+++ database/migrations/2024_03_18_155810_change_image_path_to_posts.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->longText('image_path_1')->nullable();
+            $table->longText('image_path_2')->nullable();
+            $table->longText('image_path_3')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('image_path_1');
+            $table->dropColumn('image_path_2');
+            $table->dropColumn('image_path_3');
+            $table->string('image_path_1')->nullable();
+            $table->string('image_path_2')->nullable();
+            $table->string('image_path_3')->nullable();
+        });
+    }
+};
